d02fe7cb0bfa0fa029697d001b1217978a5415e2
Stephen Fallon <sfallon92@gmail.com>
2022-04-22 21:03:53 -0400

    Adding claim differently, but still doesn't work.

--- src/FantasyCritic.Lib/Identity/FantasyCriticSignInManager.cs
+++ src/FantasyCritic.Lib/Identity/FantasyCriticSignInManager.cs
@@ -9,93 +9,30 @@ using Microsoft.Extensions.Options;
 namespace FantasyCritic.Lib.Identity;
 public class FantasyCriticSignInManager : SignInManager<FantasyCriticUser>
 {
-    private readonly IHttpContextAccessor _contextAccessor;
-
     public FantasyCriticSignInManager(UserManager<FantasyCriticUser> userManager, IHttpContextAccessor contextAccessor,
         IUserClaimsPrincipalFactory<FantasyCriticUser> claimsFactory, IOptions<IdentityOptions> optionsAccessor,
         ILogger<SignInManager<FantasyCriticUser>> logger, IAuthenticationSchemeProvider schemes, IUserConfirmation<FantasyCriticUser> confirmation)
         : base(userManager, contextAccessor, claimsFactory, optionsAccessor, logger, schemes, confirmation)
     {
-        _contextAccessor = contextAccessor;
+
     }
 
-    public override Task<SignInResult> PasswordSignInAsync(FantasyCriticUser user, string password, bool isPersistent, bool lockoutOnFailure)
+    public override Task SignInWithClaimsAsync(FantasyCriticUser user, bool isPersistent, IEnumerable<Claim> additionalClaims)
     {
-        var result = base.PasswordSignInAsync(user, password, isPersistent, lockoutOnFailure);
-        AddClaim();
-        return result;
-    }
-
-    public override Task<SignInResult> PasswordSignInAsync(string userName, string password, bool isPersistent, bool lockoutOnFailure)
-    {
-        var result = base.PasswordSignInAsync(userName, password, isPersistent, lockoutOnFailure);
-        AddClaim();
-        return result;
-    }
-
-    public override Task<SignInResult> ExternalLoginSignInAsync(string loginProvider, string providerKey, bool isPersistent)
-    {
-        var result = base.ExternalLoginSignInAsync(loginProvider, providerKey, isPersistent);
-        AddClaim();
-        return result;
-    }
-
-    public override Task<SignInResult> ExternalLoginSignInAsync(string loginProvider, string providerKey, bool isPersistent, bool bypassTwoFactor)
-    {
-        var result = base.ExternalLoginSignInAsync(loginProvider, providerKey, isPersistent, bypassTwoFactor);
-        AddClaim();
-        return result;
-    }
-
-    public override Task RefreshSignInAsync(FantasyCriticUser user)
-    {
-        var result = base.RefreshSignInAsync(user);
-        AddClaim();
-        return result;
-    }
-
-    public override Task SignInAsync(FantasyCriticUser user, bool isPersistent, string? authenticationMethod = null)
-    {
-        var result = base.SignInAsync(user, isPersistent, authenticationMethod);
-        AddClaim();
-        return result;
-    }
-
-    public override Task SignInAsync(FantasyCriticUser user, AuthenticationProperties authenticationProperties, string? authenticationMethod = null)
-    {
-        var result = base.SignInAsync(user, authenticationProperties, authenticationMethod);
-        AddClaim();
-        return result;
-    }
-
-    public override Task<SignInResult> TwoFactorAuthenticatorSignInAsync(string code, bool isPersistent, bool rememberClient)
-    {
-        var result = base.TwoFactorAuthenticatorSignInAsync(code, isPersistent, rememberClient);
-        AddClaim();
-        return result;
-    }
-
-    public override Task<SignInResult> TwoFactorRecoveryCodeSignInAsync(string recoveryCode)
-    {
-        var result = base.TwoFactorRecoveryCodeSignInAsync(recoveryCode);
-        AddClaim();
-        return result;
-    }
-
-    private void AddClaim()
-    {
-        var identity = _contextAccessor.HttpContext?.User.Identities.FirstOrDefault();
-        if (identity is null)
+        if (!additionalClaims.Any(x => x.Type == "scope" && x.Value == IdentityServerConstants.LocalApi.ScopeName))
         {
-            return;
+            additionalClaims = additionalClaims.Concat(new List<Claim>() { new Claim("scope", IdentityServerConstants.LocalApi.ScopeName) });
         }
+        return base.SignInWithClaimsAsync(user, isPersistent, additionalClaims);
+    }
 
-        var existingClaim = identity.Claims.Any(x => x.Type == "scope" && x.Value == IdentityServerConstants.LocalApi.ScopeName);
-        if (existingClaim)
+    public override Task SignInWithClaimsAsync(FantasyCriticUser user, AuthenticationProperties authenticationProperties,
+        IEnumerable<Claim> additionalClaims)
+    {
+        if (!additionalClaims.Any(x => x.Type == "scope" && x.Value == IdentityServerConstants.LocalApi.ScopeName))
         {
-            return;
+            additionalClaims = additionalClaims.Concat(new List<Claim>() { new Claim("scope", IdentityServerConstants.LocalApi.ScopeName) });
         }
-
-        identity.AddClaim(new Claim("scope", IdentityServerConstants.LocalApi.ScopeName));
+        return base.SignInWithClaimsAsync(user, authenticationProperties, additionalClaims);
     }
 }
